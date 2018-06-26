223e3f18a91ae645467e3d46e49c3fc330673557
GalV <galvigoda@gmail.com>
2018-06-26 18:04:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/galv/exame/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.galv.exame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//bla bla bla
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
