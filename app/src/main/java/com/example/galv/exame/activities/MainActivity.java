98141623f4d6b91e60df31ac9ce82e7a9f7ef78c
Naor David <naordavid2@gmail.com>
2018-08-06 13:44:31 +0300

    Added Authentication with Google and start of db

--- app/src/main/java/com/example/galv/exame/activities/MainActivity.java
+++ app/src/main/java/com/example/galv/exame/activities/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.galv.exame.activities;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.galv.exame.R;
+import com.example.galv.exame.entities.Question;
+import com.example.galv.exame.handlers.AuthenticationHandler;
+import com.example.galv.exame.handlers.DatabaseHandler;
+import com.example.galv.exame.handlers.Logger;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-//bla bla bla
+    DatabaseHandler mDatabaseHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDatabaseHandler = new DatabaseHandler();
     }
 }
